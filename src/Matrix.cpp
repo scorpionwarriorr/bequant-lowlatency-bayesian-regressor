cf86cb1a95dadd47715768b79474b2cc554bb6d2
Anshul Bhan <bhan.anshul@gmail.com>
2026-01-12 22:08:58 +0530

    Final with inverse

--- src/Matrix.cpp
+++ src/Matrix.cpp
@@ -1,6 +1,7 @@
 #include "Matrix.hpp"
 #include <iostream>
 #include <cassert>
+#include <cmath>
 Matrix Matrix::transpose() const
 {
     Matrix T(this->cols(),this->row());
@@ -37,5 +38,67 @@ Matrix Matrix::multiply(const Matrix& other) const
 
 Matrix Matrix::inverse() const
 {
-    
+    assert(this->row()==this->cols());//inverse only for square matrices
+    size_t n = this->row();
+
+    //Now the augmented matrix [A|I]
+    Matrix aug(n,2*n);
+
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            aug(i,j)=(*this)(i,j);
+        }
+    }//left hand side of the aug matrix
+
+    for(int i=0;i<n;i++)
+    {
+        for(int j=n;j<2*n;j++)
+        {
+            if(i==j-n)
+            {
+                aug(i,j)=1.0;
+            }
+            else
+            {
+                aug(i,j)=0.0;
+            }
+        }
+    }//putting the identity matrix on the right hand side
+
+    //Now we use "gaussian elimination"
+    for(int i=0;i<n;i++)
+    {
+        double ele=aug(i,i);
+        assert(std::abs(ele) > 1e-12);// to ensure no division by 0
+        for(int j=0;j<2*n;j++)
+        {
+            aug(i,j)=aug(i,j)/ele;
+        }
+        for(int r=0;r<n;r++)
+        {
+            if(r==i)
+            {
+                continue;
+            }
+            double diff=aug(r,i);
+            for(int c=0;c<2*n;c++)
+            {
+                aug(r,c)=aug(r,c)-diff*aug(i,c);
+            }
+        }
+    }
+    Matrix inv(n,n);
+        for(int i=0;i<n;i++)
+        {
+            for(int j=n;j<2*n;j++)
+            {
+                inv(i,j-n)=aug(i,j);
+            }
+        }
+    return inv;
+
 }
+
+
