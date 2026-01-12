ad7a21e3247c806cc6cc9f6359feaaa02560ff63
Anshul Bhan <bhan.anshul@gmail.com>
2026-01-12 22:09:51 +0530

    Final with inverse

--- src/OLS.cpp
+++ src/OLS.cpp
@@ -6,10 +6,24 @@
 void OLS::fit(const Matrix&X,const Matrix& y)
 {
     assert(X.row()==y.row());
+    assert(y.cols()==1);
     Matrix XT=X.transpose();
     Matrix temp=XT.multiply(X);
     temp=temp.inverse();
     temp=temp.multiply(XT);
     this->beta=temp.multiply(y);//The OLS object calls the function so we need to set for that model beta 
     fitted=true;
+}
+
+Matrix OLS::predict(const Matrix&X) const
+{
+    assert(fitted);
+    Matrix T=X.multiply(this->beta);
+    return T;
+}
+
+const Matrix& OLS::coefficients() const
+{
+    return this->beta;
+    //just for access and see it, read only cause const
 }
